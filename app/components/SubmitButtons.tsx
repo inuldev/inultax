e459646d61f99c512910f6bff5431cfb6db848f5
inuldev <inuldev0@gmail.com>
2025-06-14 01:54:55 +0700

    project setup

---
+++ app/components/SubmitButtons.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Loader2 } from "lucide-react";
+import { useFormStatus } from "react-dom";
+
+import { Button } from "@/components/ui/button";
+
+interface iAppProps {
+  text: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | null
+    | undefined;
+}
+
+export function SubmitButton({ text, variant }: iAppProps) {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-full" variant={variant}>
+          <Loader2 className="size-4 mr-2 animate-spin" /> Please wait...
+        </Button>
+      ) : (
+        <Button type="submit" className="w-full" variant={variant}>
+          {text}
+        </Button>
+      )}
+    </>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,43 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { SubmitButton } from "../components/SubmitButtons";
+
+export default async function Login() {
+  return (
+    <>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9D9E9,transparent)]" />
+      </div>
+      <div className="flex h-screen w-full items-center justify-center px-4">
+        <Card className="max-w-sm min-w-xs lg:min-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>Enter your email below to login</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form className="flex flex-col gap-y-4">
+              <div className="flex flex-col gap-y-2">
+                <Label>Email</Label>
+                <Input
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="hello@inultax.com"
+                />
+              </div>
+              <SubmitButton text="Login" />
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+}
